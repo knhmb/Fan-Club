a9d41b5f4749401201da535a0a9ec491614e9083
knhmb <khaled@marvelconnect.com>
2022-05-17 17:34:03 +0800

    Modifications

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <the-header></the-header>
+  <Banner />
+  <Promotion />
+  <Explore />
+  <Featured />
+  <Product />
+  <get-started></get-started>
+  <the-footer></the-footer>
+</template>
+
+<script>
+import TheHeader from "../components/common/TheHeader.vue";
+import Banner from "../components/home/Banner.vue";
+import Promotion from "../components/home/Promotion.vue";
+import Explore from "../components/home/Explore.vue";
+import Featured from "../components/home/Featured.vue";
+import Product from "../components/home/Product.vue";
+import GetStarted from "../components/home/GetStarted.vue";
+import TheFooter from "../components/common/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    Banner,
+    Promotion,
+    Explore,
+    Featured,
+    Product,
+    GetStarted,
+    TheFooter,
+  },
+};
+</script>
