c4074b01798351d98f6655dd3496d4a977ef4e13
knhmb <khaled@marvelconnect.com>
2022-05-18 13:32:45 +0800

    Completed theplan page

--- src/components/plan/PlanTab.vue
+++ src/components/plan/PlanTab.vue
@@ -2,12 +2,125 @@
   <div class="plan-tab">
     <base-container>
       <h3>Select a membership level</h3>
-      <el-row>
-        <el-col :sm="12" :md="6">
+      <el-row :gutter="20">
+        <el-col :sm="12" :md="8">
           <div class="plan-card">
             <p class="plan-name">Plan name - Level 1</p>
             <img src="../../assets/plan01@2x.jpg" alt="" />
             <p class="price">HKD 300</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
+          </div>
+        </el-col>
+        <el-col :sm="12" :md="8">
+          <div class="plan-card">
+            <p class="plan-name">Plan name - Level 2</p>
+            <img src="../../assets/plan02@2x.jpg" alt="" />
+            <p class="price">HKD 600</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
+          </div>
+        </el-col>
+        <el-col :sm="12" :md="8">
+          <div class="plan-card">
+            <p class="plan-name">Plan name - Level 3</p>
+            <img src="../../assets/plan03@2x.jpg" alt="" />
+            <p class="price">HKD 900</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
+          </div>
+        </el-col>
+        <el-col :sm="12" :md="8">
+          <div class="plan-card">
+            <p class="plan-name">Plan name - Level 4</p>
+            <img src="../../assets/plan04@2x.jpg" alt="" />
+            <p class="price">HKD 1500</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
+          </div>
+        </el-col>
+        <el-col :sm="12" :md="8">
+          <div class="plan-card">
+            <p class="plan-name">Plan name - Level 5</p>
+            <img src="../../assets/plan05@2x.jpg" alt="" />
+            <p class="price">HKD 2000</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
+          </div>
+        </el-col>
+        <el-col :sm="12" :md="8">
+          <div class="plan-card">
+            <p class="plan-name">Plan name - Level 6</p>
+            <img src="../../assets/plan06@2x.jpg" alt="" />
+            <p class="price">HKD 4000</p>
+            <p>PER MONTH</p>
+            <el-button>Join</el-button>
+            <p class="save">or save 10% if you pay annually</p>
+            <p class="description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque vel varius ac massa iaculis arcu lorem enim. Mi ut
+              neque egestas nunc. Platea et amet mauris, at ultricies
+              pellentesque lectus in. Tellus nulla ultricies nulla natoque lorem
+              porttitor. Elementum elementum enim mi nunc scelerisque ridiculus
+              venenatis. Habitasse pulvinar vel senectus
+            </p>
+            <p class="show-more">Show more</p>
+            <!-- <div class="hide-text">jhdkfhjsd</div> -->
           </div>
         </el-col>
       </el-row>
@@ -20,6 +133,10 @@
   padding: 3rem 0;
 }
 
+.plan-tab .el-col {
+  margin-bottom: 1rem;
+}
+
 .plan-tab h3 {
   font-family: "Inter";
   font-style: normal;
@@ -29,6 +146,7 @@
   letter-spacing: -0.02em;
   color: #292929;
   text-align: center;
+  margin-bottom: 5rem;
 }
 
 .plan-tab .plan-card {
@@ -62,5 +180,88 @@
   line-height: 32px;
   font-feature-settings: "liga" off;
   color: #292929;
+  margin: 0.5rem 0;
+}
+
+.plan-tab .plan-card p {
+  font-family: "PingFang HK";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  font-feature-settings: "liga" off;
+  color: #6f6f6f;
+}
+
+.plan-tab .plan-card .el-button {
+  background: #0faaec;
+  border-radius: 80px;
+  width: 60%;
+  font-family: "PingFang HK";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  font-feature-settings: "liga" off;
+  color: #ffffff;
+  padding: 1.2rem;
+  margin-top: 0.5rem;
+}
+
+.plan-tab .plan-card p.save {
+  font-family: "PingFang HK";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  font-feature-settings: "liga" off;
+  color: #0faaec;
+  text-decoration: underline;
+  margin-top: 0.5rem;
+}
+
+.plan-tab .plan-card p.description {
+  max-height: 100px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: relative;
+  margin-top: 1rem;
+}
+
+.plan-tab .plan-card p.description:before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: linear-gradient(
+    0deg,
+    #ffffff 0%,
+    rgba(255, 255, 255, 0.5) 51.56%,
+    rgba(217, 217, 217, 0) 100%,
+    rgba(255, 255, 255, 0) 100%
+  );
+}
+
+.plan-tab .plan-card p.show-more {
+  background: none;
+  border: none;
+  margin: 0;
+  font-family: "PingFang HK";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  font-feature-settings: "liga" off;
+  color: #0faaec;
+  border-bottom: 1px solid #0faaec;
+  border-radius: 0;
+  width: fit-content;
+  margin: 0 auto;
+}
+
+.plan-tab .plan-card p.show-more:hover {
+  cursor: pointer;
 }
 </style>
